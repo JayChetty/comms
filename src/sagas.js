05914ea2f51a20afcaa5f4f8428dc6f07e7c232a
Jay Chetty <jaypaulchetty@gmail.com>
2017-04-19 12:07:47 +0100

    tidy

--- src/sagas.js
+++ src/sagas.js
@@ -10,40 +10,31 @@ export function* incrementAsync() {
 }
 
 export function* signInSubmit( action ){
-  console.log("signInSubmit", action)
   let response = yield call( firebaseSignin, action )
-  console.log("response", response)
   if(response.error){
-    console.log( "got error", response.error )
     yield put({ type: "SET_AUTH_ERROR", error: response.error.message })
   }else{
-    console.log( "got user", response.user )
     yield put({ type: "SET_USER", user: response.user })
   }
 }
 
 export function* getCurrentUser(){
     let currentUser = yield call( firebaseGetCurrentUser )
-    console.log("got the user man", currentUser)
     if( currentUser ){
       yield put({ type: "SET_USER", user: currentUser })
     }
 }
 
-
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 export function* watchIncrementAsync() {
-  console.log("watch inc async")
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
 
 export function* watchSignInSubmit() {
-  console.log("watch signin submit")
   yield takeEvery('SIGNIN_SUBMIT', signInSubmit)
 }
 
 export function* watchGetCurrentUser() {
-  console.log("watch get current user")
   yield takeEvery('GET_CURRENT_USER', getCurrentUser)
 }
 
