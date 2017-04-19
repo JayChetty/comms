cdfdd54a9968e385d2ffc76fd857035e2a4e0400
Jay Chetty <jaypaulchetty@gmail.com>
2017-04-19 16:42:41 +0100

    investigate advanced options

--- src/sagas.js
+++ src/sagas.js
@@ -1,5 +1,5 @@
 import { delay } from 'redux-saga'
-import { put, call, takeEvery } from 'redux-saga/effects'
+import { take, put, call, takeEvery } from 'redux-saga/effects'
 
 import {firebaseGetCurrentUser, firebaseSignin } from './firebase_helpers'
 
@@ -9,6 +9,7 @@ export function* incrementAsync() {
   yield put({ type: 'INCREMENT' })
 }
 
+//(Intuition) These yield out Promises to the watchers that then in turn feedback to them the resolved result
 export function* signInSubmit( action ){
   let response = yield call( firebaseSignin, action )
   if(response.error){
@@ -18,6 +19,9 @@ export function* signInSubmit( action ){
   }
 }
 
+
+//The watcher will trigger d.throw if .catch.  This will enter any catch block in here. So can do error checking
+//in here.
 export function* getCurrentUser(){
     let currentUser = yield call( firebaseGetCurrentUser )
     if( currentUser ){
@@ -26,9 +30,30 @@ export function* getCurrentUser(){
 }
 
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+
+//(Intuition) The watchers are calling saga.next() and passing in the information from the returned promise.
+//(This is all happening behind the scene)
 export function* watchIncrementAsync() {
+  //Queue up all the increments and allow them to happen onw after another(typical js!)
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+  //Only one can happen at a time, latest one will replace the others(traffic lights)
+  // yield takeLatest('INCREMENT_ASYNC', incrementAsync)
 }
+
+
+//Can do all kinds of funky stuff like this - Could use a for loop and then we can only listen for a certain amount
+//of actions.  THIS IS THE SHINDIG!
+// export function* watchIncrementAsync() {
+//   //Queue up all the increments and allow them to happen onw after another
+//   while(true){
+//     console.log("waiting to get action")
+//     const action = yield take('INCREMENT_ASYNC')//this will now suspend the generator until the promise resolves
+//     console.log("got action setting off delay")
+//     yield call(delay, 1000)
+//     console.log("heard back form delay")
+//     yield put({ type: 'INCREMENT' })
+//   }
+// }
 
 export function* watchSignInSubmit() {
   yield takeEvery('SIGNIN_SUBMIT', signInSubmit)
