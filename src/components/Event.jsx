ef0b5172c474c902d6745bd31cd8555fa33783c1
Jay Chetty <jaypaulchetty@gmail.com>
2017-05-22 15:03:12 +0100

    show other users guesses

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,11 +3,30 @@ import { connect } from 'react-redux'
 import Form from './Form'
 import { firebaseUpdateValue } from '../firebase_helpers'
 
-function Event({dispatch, event, group, submission, updateSubmission}){
+function Event({dispatch, event, group, submission, updateSubmission, submissions, currentUserId}){
+  console.log("Event group", group)
+  const forms = Object.keys(group.members).map((memberId)=>{
+    const member = group.members[memberId]
+    const submission = submissions[memberId]
+    const isCurrentUser = currentUserId === memberId
+    return (
+    <div>
+      <h3>{member.displayName}</h3>
+      <Form
+        key={memberId}
+        form={event.form}
+        submission={submission}
+        onFormChange={updateSubmission}
+        isCurrentUser={isCurrentUser}>
+      </Form>
+    </div>
+  )
+  })
+
   return (
     <div className="App">
       {event.name}
-      <Form form={event.form} submission={submission} onFormChange={updateSubmission}></Form>
+      {forms}
     </div>
   );
 }
@@ -16,6 +35,8 @@ const mapStateToProps = (state, {match}) =>{
   const event = state.events[match.params.eventId]
   const group = state.groups[match.params.groupId]
   //need to get submission from the group event
+  const submissions = group.events[match.params.eventId].submissions
+  const currentUserId = state.user.uid
   const submission = group.events[match.params.eventId].submissions[state.user.uid]
 
   const updateSubmission = (value, formCategory)=>{
@@ -28,7 +49,9 @@ const mapStateToProps = (state, {match}) =>{
     event,
     group,
     submission,
-    updateSubmission
+    updateSubmission,
+    submissions,
+    currentUserId
   }
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,17 +7,28 @@ import './App.css';
 // } from 'react-router-dom'
 
 
-export default function Form( { form, submission, onFormChange } ) {
+export default function Form( { form, submission, onFormChange, isCurrentUser } ) {
   console.log("form", submission)
+  if(!submission && isCurrentUser){//setup form from default
+    Object.keys(form.default).forEach((formKey)=>{
+      onFormChange(form.default[formKey], formKey)
+    })
+  }
   const dataSource = submission || form.default
   // // const events = props.events
   const formListItems = Object.keys(dataSource).map((formKey)=>{
     // return ( <li key={formKey}> { formKey } </li> )
     dataSource[formKey]
+    let input = null
+    if(isCurrentUser){
+      input = <input onChange={ (event)=>onFormChange(event.target.value, formKey) } value={dataSource[formKey]} name={formKey} type='text'/>
+    }else{
+      input = <input readOnly value={dataSource[formKey]} name={formKey} type='text'/>
+    }
     return(
       <label key={formKey}>
         {formKey}:
-        <input onChange={ (event)=>onFormChange(event.target.value, formKey) } value={dataSource[formKey]} name={formKey} type='text'/>
+        { input }
       </label>
     )
   })
