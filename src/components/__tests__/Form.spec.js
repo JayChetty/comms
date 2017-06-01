68c2ca1858ea5e931d888ca14315bc8092c2bf7e
Jay Chetty <jaypaulchetty@gmail.com>
2017-06-01 11:52:25 +0100

    setup tests

---
+++ src/components/__tests__/Form.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../Form';
+import renderer from 'react-test-renderer';
+it('renders correctly', () => {
+})
+// it('renders correctly', () => {
+//   const tree = renderer.create(
+//     <Form
+//       form={{default:{}}}
+//     >
+//     </Form>
+//   ).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
